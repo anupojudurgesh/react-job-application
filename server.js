5c645b52e5cfc4f8dcfd68393c538a22fab6cf3c
anupojudurgesh <anupojudurgesh@gmail.com>
2024-08-06 11:39:47 +0530

    React_Job

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+const jsonServer = require('json-server');
+const path = require('path');
+const express = require('express');
+const app = express();
+const router = jsonServer.router(path.join(__dirname, 'jobs.json'));
+const middlewares = jsonServer.defaults();
 
-
-const jsonServer = require("json-server")
-const server = jsonServer.create();
-const router = jsonServer.router("./src/jobs.json")
-const middlewares = jsonServer.defaults({
-    static : "./build"
-})
-
-
-server.use(middlewares);
-server.use('/api', router);
 const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+
+app.use('/api', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
